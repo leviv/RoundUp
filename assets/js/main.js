f55ad9da70e9887a4b66bf724474ee1dee897633
Levi V <villarreallevi@gmail.com>
2018-02-24 18:12:12 -0600

    Base Styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,25 @@
-$( window ).resize(function() {
-    $window = $(window);
-    if( $window .width() > 767){
+function animateWithRandomNumber(myClass, from, to) {
+	TweenLite.fromTo(myClass, Math.floor(Math.random() * 20 + 1), { y: from }, { y: to,
+																				onComplete: animateWithRandomNumber,
+																				onCompleteParams: [myClass, from, to],
+																				ease: Linear.easeNone });
+}
 
-        $('section[data-type="background"]').each(function(){
-            var $bgobj = $(this); // assigning the object
-
-            $(window).scroll(function() {
-
-                // Scroll the background at var speed
-                // the yPos is a negative value because we're scrolling it UP!                              
-                var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
-
-                // Put together our final background position
-                var coords = '50% '+ yPos + 'px';
-
-                // Move the background
-                $bgobj.css({ backgroundPosition: coords });
-
-            }); // window scroll Ends
-
-        });    
-    }
+var itemsDown = [".light4", ".light5", ".light6", ".light7", ".light8", ".light11", ".light12", ".light13", ".light14", ".light15", ".light16"].forEach(function (e) {
+	return animateWithRandomNumber(e, -1080, 1080);
+});
+var itemsUp = [".light1", ".light2", ".light3", ".light9", ".light10", ".light17"].forEach(function (e) {
+	return animateWithRandomNumber(e, 1080, -1080);
 });
 
 
+// Bouncing arrow bottom of full screen
+// Different arrow choices from Font Awesome
+// Fades away as you scroll down
 
-$(document).ready(function(){
-    $window = $(window);
-    if( $window.width() > 767){
-        // Cache the Window object
-
-        $('section[data-type="background"]').each(function(){
-            var $bgobj = $(this); // assigning the object
-
-            $(window).scroll(function() {
-
-                // Scroll the background at var speed
-                // the yPos is a negative value because we're scrolling it UP!                              
-                var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
-
-                // Put together our final background position
-                var coords = '50% '+ yPos + 'px';
-
-                // Move the background
-                $bgobj.css({ backgroundPosition: coords });
-
-            }); // window scroll Ends
-
-        });    
-    }
-});
-
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 40) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");       
-        $(".nav-brand").addClass("logo-shrink");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+$(window).scroll(function(){
+	$(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+	//250 is fade pixels
 });
 
 /* ===============================
@@ -99,31 +59,3 @@ $(document).ready(function(){
         });
     });
 });
-
-
-
-
-
-/* ===============================
-    Tabbable Content
-    http://callmenick.com/2015/01/25/animating-css-only-hamburger-menu-icons/
-=============================== */
-(function () {
-
-    "use strict";
-
-    var toggles = document.querySelectorAll(".cmn-toggle-switch");
-
-    for (var i = toggles.length - 1; i >= 0; i--) {
-        var toggle = toggles[i];
-        toggleHandler(toggle);
-    };
-
-    function toggleHandler(toggle) {
-        toggle.addEventListener( "click", function(e) {
-            e.preventDefault();
-            (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
-        });
-    }
-
-})();
